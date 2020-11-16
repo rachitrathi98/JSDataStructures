ae7c334488c3b8969086b981592df741f2c89c46
Rachit Rathi <rachitrathi98@gmail.com>
2020-11-16 23:20:31 +0530

    [Rachit] Add. Refactor Code for dictionary

--- Maps/MapsP1.js
+++ Maps/MapsP1.js
@@ -2,21 +2,20 @@
 var diceDict = new Object();
 var diceDict = {};
 let count=0;
+let diceNum=0;
 while(true)
     {
-        let diceNum=Math.floor(Math.random() * 6) + 1;
+        if(diceDict[diceNum]>9){break;}
+        else
+        {
+         diceNum=Math.floor(Math.random() * 6) + 1;
          if (!diceDict.hasOwnProperty(diceNum)) {
             diceDict[diceNum] = 1;
           }
           else{
-            if(diceDict[diceNum]>9){
-                break;
-            }
-            else
-            {
             diceDict[diceNum]++;
-            }
           }
+        }
      }
      for(var key in diceDict){
          if(diceDict[key]==10){
